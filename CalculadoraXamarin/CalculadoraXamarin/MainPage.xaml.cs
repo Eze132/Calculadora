2d7ba7c6b750c558b94a5636b7645693d0ad66aa
Ezequiel Pereira da Silva <kiel.btu@hotmail.com>
2019-09-16 17:38:28 -0300

    Adicionar arquivos de projeto.

---
+++ CalculadoraXamarin/CalculadoraXamarin/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CalculadoraXamarin
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonSoma_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ButtonMultiplicar_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ButtonSubtrair_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ButtonApagar_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ButtonIgual_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ButtonDividir_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
