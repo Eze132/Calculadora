16d38eacd78aa6474a8e978a5fca92106e55ad72
Ezequiel Pereira da Silva <kiel.btu@hotmail.com>
2019-09-17 14:00:39 -0300

    Segunda Atualização Ezequiel

---
+++ CalculadoraXamarin/CalculadoraXamarin/Contas.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalculadoraXamarin
+{
+    class Contas
+    {
+        public void Soma(int n1, int n2)
+        {
+         n1 =  n1 + n2;
+        }
+
+        public void Subtrair(int n1, int n2)
+        {
+            n1 = n1 - n2;
+        }
+        public void Multiplicar(int n1, int n2)
+        {
+            n1 = n1 + n2;
+        }
+
+        public void Dividir(int n1, int n2)
+        {
+            n1 = n1 + n2;
+        }
+    }
+}
--- CalculadoraXamarin/CalculadoraXamarin/MainPage.xaml.cs
+++ CalculadoraXamarin/CalculadoraXamarin/MainPage.xaml.cs
@@ -43,5 +43,55 @@ namespace CalculadoraXamarin
         {
 
         }
+
+        private void ButtonUm_Clicked(object sender, EventArgs e)
+        {
+            entryNumero.Text = Convert.ToString(1);
+        }
+
+        private void ButtonDois_Clicked(object sender, EventArgs e)
+        {
+            entryNumero.Text = Convert.ToString(2);
+        }
+
+        private void ButtonTres_Clicked(object sender, EventArgs e)
+        {
+            entryNumero.Text = Convert.ToString(3);
+        }
+
+        private void ButtonQuatro_Clicked(object sender, EventArgs e)
+        {
+            entryNumero.Text = Convert.ToString(4);
+        }
+
+        private void ButtonCinco_Clicked(object sender, EventArgs e)
+        {
+            entryNumero.Text = Convert.ToString(5);
+        }
+
+        private void ButtonSeis_Clicked(object sender, EventArgs e)
+        {
+            entryNumero.Text = Convert.ToString(6);
+        }
+
+        private void ButtonSete_Clicked(object sender, EventArgs e)
+        {
+            entryNumero.Text = Convert.ToString(7);
+        }
+
+        private void ButtonOito_Clicked(object sender, EventArgs e)
+        {
+            entryNumero.Text = Convert.ToString(8);
+        }
+
+        private void ButtonNove_Clicked(object sender, EventArgs e)
+        {
+            entryNumero.Text = Convert.ToString(9);
+        }
+
+        private void ButtonZero_Clicked(object sender, EventArgs e)
+        {
+            entryNumero.Text = Convert.ToString(0);
+        }
     }
 }
